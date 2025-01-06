1eb76c5bc90469af1c9b902e1c019268cb1438ca
Fernando Babonoyaba <fernando.babonoyaba@gmail.com>
2025-01-06 09:48:02 -0700

    d commit

---
+++ jsreview.js
@@ -0,0 +1,84 @@
+//  Variables & Datatypes
+
+/*
+A. Q +A
+1. How do we assign a value to a variable?
+let x = 20
+2. How do we change the value of a variable?
+x = 5
+3. How do we assign an existing variable to a new variable?
+let y = x
+4. Remind me, what are declare , assign , and define ?
+let declare =(assign) define
+5. What is pseudocoding and why should you do it?
+sudo code is use so organise are thought in a way we can write latter
+6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+think 80 program 20
+
+
+B. Strings
+For all other questions that involve writing code, you can solve them via the following instructions.
+1. Create a variable called first Variable
+let firstVariable
+2. Assign it the value of the string "Hello World"
+firstVariable = "Hello World"
+3. Change the value of this variable to some number
+firstVariable = 5
+
+4. Store the value of first Variable in a new variable called second Variable
+let secondVariable = firstVariable
+5. Change the value of secondVariable to any string.
+secondVariable = "hi"
+6. What is the value of firstVariable ?
+5
+7. Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my
+name is " and the variable yourName so that it returns a new string with them concatenated.
+ex: Hello, my name is Jean Valjean
+*/
+
+let name = "fernando Babonoyaba"
+let expresion = `Hello, my name is ${name}`
+
+
+
+
+// Boolean
+
+
+const a = 4 ;
+const b = 53 ;
+const c = 57 ;
+const d = 16;
+const e = 'Kevin';
+console.log(a < b);
+console.log( c > d );
+console.log( 'Name' === 'Name');
+// FOR THE NEXT TWO, USE ONLY && OR 11
+console.log ( true !== false ) ;
+console.log(false === false && false === false && false !== true);
+console.log(false === false )
+console.log(e === 'Kevin');
+console.log(a < b < c ) ; // note: a < b < c i s NOT CORRECT Cand i s not a valid JS expressior
+console.log( a === a)
+console.log(48 == '48');
+
+
+//d 
+let animal = "cow"
+
+if (animal === "cow") {
+    console.log("mooooo");
+    
+}
+
+
+
+
+//
+//
+// loops
+//conditionals
+
+//easy does it
+// accessing elements
+//
